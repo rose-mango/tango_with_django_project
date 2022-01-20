6af6cfe660760ae29a7fc4f6c65d44da87e6b5eb
Alex Pronins <2569781p@student.gla.ac.uk>
2022-01-20 14:00:51 +0000

    exercises from ch 3

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    response = "<p>Rango says hey there partner!</p><br>"
+    response += "<a href='about/'>go to about</a>"
+    return HttpResponse(response)
+    
+def about(request):
+    response = "<p>Rango says here is the about page.</p><br>"
+    response += "<a href='../'>go to index</a>"
+    return HttpResponse(response)
